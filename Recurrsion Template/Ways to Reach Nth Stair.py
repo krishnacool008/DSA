93851c431fe20e4bc8049e08ea101c763d36a4a6
Krishna Chaturvedi <36785880+krishnacool008@users.noreply.github.com>
2026-08-08 17:53:31 +0530

    Climbing N Stairs

---
+++ Recurrsion Template/Ways to Reach Nth Stair.py
@@ -0,0 +1,13 @@
+# Recurrence Relation: f(i) = f(i-1) + f(i-2)
+# Constraints: 1 <= i <= 45
+def countWays(i):
+    if i <= 1:
+        return 1
+    else:
+        return countWays(i - 1) + countWays(i - 2)
+
+
+# Example usage:
+n = 6  # Number of stairs
+ways = countWays(n)
+print(f"Number of ways to reach the {n}th stair: {ways}")
