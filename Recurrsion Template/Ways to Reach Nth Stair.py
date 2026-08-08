c5d9b10b6cc997f1016d747ad2ae688ea665fd38
Krishna Chaturvedi <36785880+krishnacool008@users.noreply.github.com>
2026-08-08 18:06:46 +0530

    Modified code template

--- Recurrsion Template/Ways to Reach Nth Stair.py
+++ Recurrsion Template/Ways to Reach Nth Stair.py
@@ -1,9 +1,12 @@
+# Recurrence: f(i) represents number of ways to reach ith stair from the ground
 # Recurrence Relation: f(i) = f(i-1) + f(i-2)
 # Constraints: 1 <= i <= 45
 def countWays(i):
+    # Base Case
     if i <= 1:
         return 1
     else:
+        # Recurrence Relation
         return countWays(i - 1) + countWays(i - 2)
 
 
--- Recurrsion Template/fibonacchi.py
+++ Recurrsion Template/fibonacchi.py
@@ -1,8 +1,12 @@
+# recurrence: f(i) represents number at ith position in a fibonacci series
 # recurrence relation: f(i) = f(i-1) + f(i-2)
 # input i >= 0
 def fibonacchi(i):
+    # Base Case
     if i < 2:
         return i
+
+    # Recurrence relation
     return fibonacchi(i-1) + fibonacchi(i-2)
 
 
